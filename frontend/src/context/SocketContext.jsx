7ac306625c5f63229d7aa3ead97c3049de550b45
Deepak <deepakkumar09092003@gmail.com>
2024-08-20 13:08:35 +0530

    socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider=({children})=>{
 
     useEffect(() => {
      if(authUser){
-        const socket=io("http://localhost:4000",{
+        const socket=io("https://letustalk.onrender.com/",{
             query:{
                 userId:authUser._id,
             },
