c7b7a333838c258f1a3934f4474f7b4a6bd1b480
Deepak <deepakkumar09092003@gmail.com>
2024-08-20 14:05:31 +0530

    add responsive

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import Sidebar from "../../components/sidebar/Sidebar"
 import MessageContainer from "../../components/messages/MessageContainer"
 export default function Home() {
   return (
-    <div className="flex flex-col sm:flex-row sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10">
+    <div className="flex flex-col sm:flex-row sm:min-h-[450px] md:min-h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10">
       <Sidebar/>
       <MessageContainer/>
     </div>
